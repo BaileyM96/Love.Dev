ee422e4a159313021b0531a5328b6b3bc3da0f6e
Ileanacz <ily_cz@outlook.com>
2023-02-28 19:02:50 -0800

    Added global styles and imported it to app.js

--- love.dev/src/App.js
+++ love.dev/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import logo from './logo.svg';
 import './App.css';
 import GlobalStyles from './Global';
@@ -23,6 +24,33 @@ function App() {
       </header>
     </div>
   );
+=======
+// Render components to build page
+
+import GlobalStyles from './components/styles/Global';
+import { ThemeProvider } from 'styled-components'
+
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333',
+  },
+  mobile: '390px',
+}
+function App() {
+  return (
+    <ThemeProvider theme = {theme}>
+      <>
+    <GlobalStyles />
+  //   <div className="App">
+     
+  //   </div>
+  </>
+  </ThemeProvider>
+  // );
+>>>>>>> Stashed changes
   )
 }
 

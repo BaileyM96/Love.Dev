27c2231fd602be88bb9ed05c35e5dc3568ad3d20
Sublime-Sweetie <faith.Wallace.2@gmail.com>
2023-03-02 19:42:31 -0800

    seeds organized, gitignore added, package.json added

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,26 +1,15 @@
 const db = require('../config/connection');
 const { User, Profile } = require('../models');
-const userSeeds = require('./userSeeds.json');
-const thoughtSeeds = require('./thoughtSeeds.json');
+const userSeeds = require('./user-Seeds.json');
+
 
 db.once('open', async () => {
   try {
-    await Profile.deleteMany({});
-    await User.deleteMany({});
+    await Profile.deleteOne({});
+    await User.deleteOne({});
 
     await User.create(userSeeds);
 
-    for (let i = 0; i < thoughtSeeds.length; i++) {
-      const { _id, thoughtAuthor } = await Profile.create(thoughtSeeds[i]);
-      const user = await User.findOneAndUpdate(
-        { username: thoughtAuthor },
-        {
-          $addToSet: {
-            thoughts: _id,
-          },
-        }
-      );
-    }
   } catch (err) {
     console.error(err);
     process.exit(1);
@@ -29,3 +18,5 @@ db.once('open', async () => {
   console.log('all done!');
   process.exit(0);
 });
+
+

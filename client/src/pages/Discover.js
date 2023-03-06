5ffd69307fbaf677cc500c738fbdda3b141e1276
Bailey Mejia <baileymejia28@gmail.com>
2023-03-05 17:16:31 -0800

    added gitignore for node modules and added the explore page and profile page.

---
+++ client/src/pages/Discover.js
@@ -0,0 +1,11 @@
+import { CardContainer } from "../components/styles/container.styled";
+import { H1 } from "../components/styles/Header.styled";
+import Card from "../components/Card";
+
+export default function Discover() {
+    return (
+        <Card>
+           
+        </Card>
+    );
+}

da2cdfd0cd76ced879e108cdc5af975940b24790
Ajaws2022 <112523117+Ajaws2022@users.noreply.github.com>
2023-03-06 19:17:09 -0800

    removed uneeded comments from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,34 +10,6 @@ function App() {
     </Avatar>
   );
 };
-// Render components to build page
-
-// import GlobalStyles from './components/styles/Global';
-// import { ThemeProvider } from 'styled-components'
-
-
-// const theme = {
-//   colors: {
-//     header: '#ebfbff',
-//     body: '#fff',
-//     footer: '#003333',
-//   },
-//   mobile: '390px',
-// }
-// function App() {
-//   return (
-//     <ThemeProvider theme = {theme}>
-//       <>
-//     <GlobalStyles />
-//   //   <div className="App">
-     
-//   //   </div>
-//   </>
-//   </ThemeProvider>
-//   // );
-
-//   )
-// }
 
 
 

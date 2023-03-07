3f7774cbe42bba7f96cb60a16167b7260bcec9ae
Ajaws2022 <112523117+Ajaws2022@users.noreply.github.com>
2023-03-06 19:35:25 -0800

    removed double container instance in container.styled

--- client/src/App.js
+++ client/src/App.js
@@ -14,12 +14,7 @@ import Avatar from './components/Avatar'
 
 function App() {
   return (
-    <div style={{background: 'linear-gradient(175deg, rgba(218,77,141,1) 39%, rgba(115,10,244,1) 83%)', width: '100%', height: '100%'}}>
-       {/* <Landing /> */}
-      <Discover />
-      {/* <ProfileComponent /> */}
-      {/* <Matches /> */}
-    </div>
+    
     <Avatar>
 
     </Avatar>

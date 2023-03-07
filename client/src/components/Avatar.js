456943b1930db50844647096b4891d98bd362c0c
Ajaws2022 <112523117+Ajaws2022@users.noreply.github.com>
2023-03-06 20:40:35 -0800

    added functionality to avatar page

--- client/src/components/Avatar.js
+++ client/src/components/Avatar.js
@@ -7,30 +7,44 @@ import AvatarB from "../Avatars/AvatarB.png";
 import AvatarC from "../Avatars/AvatarC.png";
 import AvatarD from "../Avatars/AvatarD.png";
 import AvatarE from "../Avatars/AvatarE.png";
-
-
-const avatarPicker = () => {
-    console.log('photoadded to userState')
-};
+import { UPDATE_USER } from '../utils/mutations';
+import { useMutation  } from '@apollo/client';
+import { getProfile } from '../utils/auth'
 
 export default function Avatar() {
+        const user = getProfile();
+
+        const [updateImage, {loading}] = useMutation(UPDATE_USER);
+    
+        const imageUpload = (event) => {
+            const image = event.target.files[0];
+            const formData = new FormData();
+            formData.append('image', image);
+    
+            updateImage({
+                variables: {id: user.id, }
+            });
+        };
+            
+    
+    
     return (
         <div>
             <H1>Pick Your Avatar</H1>
         <LandingContainer>
-            <PhotoButton><img src={AvatarA} alt="Avatar A" onClick={avatarPicker}>
+            <PhotoButton><img src={AvatarA} alt="Avatar A" onClick={imageUpload}>
                 </img> 
             </PhotoButton>
-            <PhotoButton><img src={AvatarB} alt="Avatar B" onClick={avatarPicker}>
+            <PhotoButton><img src={AvatarB} alt="Avatar B" onClick={imageUpload}>
                 </img> 
             </PhotoButton>
-            <PhotoButton><img src={AvatarC} alt="Avatar C" onClick={avatarPicker}>
+            <PhotoButton><img src={AvatarC} alt="Avatar C" onClick={imageUpload}>
                 </img> 
             </PhotoButton>
-            <PhotoButton><img src={AvatarD} alt="Avatar D" onClick={avatarPicker}>
+            <PhotoButton><img src={AvatarD} alt="Avatar D" onClick={imageUpload}>
                 </img> 
             </PhotoButton>
-            <PhotoButton><img src={AvatarE} alt="Avatar E" onClick={avatarPicker}>
+            <PhotoButton><img src={AvatarE} alt="Avatar E" onClick={imageUpload}>
                 </img> 
             </PhotoButton>
 
@@ -40,4 +54,6 @@ export default function Avatar() {
         
     );
 };
+
+//Uncomment this later
 

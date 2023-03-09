12e65991ef790a55e1d8ed400fa2ce340f02d5fc
Bailey Mejia <baileymejia28@gmail.com>
2023-03-08 18:49:00 -0800

    made changes to packages.

--- client/src/App.js
+++ client/src/App.js
@@ -22,8 +22,8 @@ function App() {
   return (
     <ApolloProvider client={client}>
     <div style={{background: 'linear-gradient(175deg, rgba(218,77,141,1) 39%, rgba(115,10,244,1) 83%)', width: '100%', height: '100%'}}>
-       {/* <Landing /> */}
-      <Discover />
+       <Landing />
+      {/* <Discover /> */}
       {/* <ProfileComponent /> */}
       {/* <Matches /> */}
       {/* <Avatar>
--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -10,17 +10,19 @@ import Profiled from "../components/Profile";
 import { QUERY_USERS } from '../utils/queries';
 
 // Added comment
-export default function Card({ name }) {
+export default function Card() {
 
     // State variables 
     const [showProfilePage, setProfilePage] = useState(false);
     const [likeUser, setLikeUser] = useState(0);
 
     const { data } = useQuery(QUERY_USERS, {
-        variables: {name},
+        variables:{ gender: 'Female' },
     });
 
-    const User = data.User;
+    const user = data?.user;
+    console.log(user)
+
 
     function handleProfile() {
         setProfilePage(true);
@@ -41,6 +43,7 @@ export default function Card({ name }) {
         <HeaderContainer>
             <H2>Explore</H2>
         </HeaderContainer>
+        
 
         <CardContainer>
             <Profile onClick={handleProfile}>
@@ -55,7 +58,7 @@ export default function Card({ name }) {
         </BigImageContainer>
 
         <NameContainer>
-           <NameItems>{User.name}</NameItems>
+            <NameItems></NameItems>
             <NameItems2>JavaScript</NameItems2>
         </NameContainer>
 
